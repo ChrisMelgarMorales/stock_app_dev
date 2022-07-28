38c3a018314ba75ff7db2c9ef8c372f49d78a2b2
Christopher Melgar <cmelgarmorales@csu.fullerton.edu>
2022-07-27 19:03:32 -0700

    python back to 3.10, api recieves data from yfin

--- amplify/backend/function/greetingfunction/src/index.py
+++ amplify/backend/function/greetingfunction/src/index.py
@@ -1,18 +1,23 @@
 import json
 import datetime
-#import yfinance as yf
+import yfinance as yf
 def handler(event, context):
 
-  #stockInfo = yf.Ticker("GOOG")
-  #dictionaryInfo = stockInfo.info
+  stockInfo = yf.Ticker("GOOG")
+  dictionaryInfo = stockInfo.info
   current_time = datetime.datetime.now().time()
 
   body = {
       'message': 'Hello, the current time is ' + str(current_time),
-      #'open': str(dictionaryInfo[0]),
-      #'close': str(dictionaryInfo[1]),
-      #'bid': str(dictionaryInfo[2])
-  }
+      'open': str(dictionaryInfo[0]),
+      'close': str(dictionaryInfo[1]),
+      'bid': str(dictionaryInfo[2]),
+      'ask':str(dictionaryInfo[3])
+      'volume': str(dictionaryInfo[4]),
+      'pegRatio': str(dictionaryInfo[5]),
+      'trailingEps': str(dictionaryInfo[6]),
+      'fowardEps': str(dictionaryInfo[7])
+    }
 
   response = {
       'statusCode': 200,
